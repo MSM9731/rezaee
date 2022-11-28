b2a79d0a0f8279a0cc221259d68531a6faf61498
MSM <salehmohammadi@chmail.ir>
2022-11-28 15:30:47 +0330

    Add libs and some change on index page

---
+++ app/views/inc_old/head.php
@@ -0,0 +1,78 @@
+<html dir="rtl" lang="fa">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link rel="stylesheet" href="<?php echo URLPATH ?>/node_modules/bootstrap/dist/css/bootstrap.rtl.min.css" />
+
+    <title>rezaee.ir</title>
+
+    <style>
+        :root {
+            --scrollbar-width: 5px;
+            --scrollbar-height: 5px;
+        }
+
+        ::-webkit-scrollbar {
+            width: var(--scrollbar-width);
+            height: var(--scrollbar-height);
+        }
+
+        ::-webkit-scrollbar-button,
+        ::-webkit-scrollbar-corner {
+            display: none;
+            background: transparent;
+        }
+
+        ::-webkit-scrollbar-thumb {
+            border-radius: 8px;
+            background-color: black;
+            background-color: #555;
+        }
+
+        ::-webkit-scrollbar-track,
+        ::-webkit-scrollbar-track-piece {
+            border-radius: 8px;
+            background-color: white;
+            background-color: #ccc;
+        }
+
+        a {
+            text-decoration: none;
+        }
+
+        .bg-white {
+            background-color: white;
+        }
+
+        .bg-whitesmoke {
+            background-color: whitesmoke;
+        }
+
+        /* .border-grey {
+            border-color: rgb(179, 175, 168);
+        } */
+
+        /* .operator .operations {
+            visibility: hidden;
+        } */
+
+        /* .operator:hover .operations {
+            visibility: visible;
+        } */
+
+        /* .dropdown-menu {
+            --scrollbar-width: 2px;
+            max-height: 200px;
+            overflow-y: scroll;
+        } */
+    </style>
+</head>
+
+<body class="bg-whitesmoke">
+    <?php load('/views/inc/navbar.php') ?>
+    <main class="">
+        <div class="container-fluid">
+            <div class="row py-4 justify-content-center align-items-start">
