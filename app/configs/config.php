2c6e1bd9f6f5a7d25257e2cab35bf579476bae2f
MSM9731 <salehmohammadi@chmail.ir>
2022-12-08 13:45:58 +0330

    some changes

--- app/configs/config.php
+++ app/configs/config.php
@@ -4,3 +4,12 @@ define('ROOT', dirname($_SERVER['PHP_SELF']));
 define('APPPATH', dirname(__DIR__));
 // define('URLPATH', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF'])); // with http(s)://
 define('URLPATH', $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF'])); // without http(s)://
+
+
+echo '<pre dir="ltr" style="padding:10px;">';
+echo ROOT;
+echo '<br>';
+echo APPPATH;
+echo '<br>';
+echo URLPATH;
+echo '</pre>';
